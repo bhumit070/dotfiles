b98dcac4f371e74a61a7742f7b99acebd8a0b5c7
bhumit070 <ganatrabhoomit070@gmail.com>
2023-03-12 13:15:35 +0530

    remove zsh nvm plugins because I use fnm now

--- setup/zsh-setup.sh
+++ setup/zsh-setup.sh
@@ -23,7 +23,6 @@ clone() {
 clone https://github.com/zsh-users/zsh-autosuggestions $pluginsDir/zsh-autosuggestions
 clone https://github.com/jeffreytse/zsh-vi-mode.git $pluginsDir/zsh-vi-mode
 clone https://github.com/zsh-users/zsh-syntax-highlighting.git $pluginsDir/zsh-syntax-highlighting
-clone https://github.com/lukechilds/zsh-nvm.git $pluginsDir/zsh-nvm
 
 # THEME INSTALL
 clone https://github.com/romkatv/powerlevel10k.git $themeDir/powerlevel10k
