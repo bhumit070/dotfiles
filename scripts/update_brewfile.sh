b3068e830274d2ac20cf0bd37b740c3403f8aeed
bhumit070 <ganatrabhoomit070@gmail.com>
2024-08-03 12:03:01 +0530

    add: script to update brewfile

---
+++ scripts/update_brewfile.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# This script updates the Homebrew formula for the current version of the
+
+FILE=$HOME/dotfiles/brew/Brewfile
+brew bundle dump --file=$FILE -f
+
+if git diff --quiet HEAD -- "$FILE"; then
+	cd $HOME/dotfiles/
+
+	git add brew/Brewfile
+	git commit -m " update: brewfile"
+
+	branch=$(git branch --show-current)
+	git push origin $branch
+fi
