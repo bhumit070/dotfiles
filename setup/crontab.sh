3cbcfaafc55e45223530839d9c40e75f4aa9f9ae
bhumit070 <ganatrabhoomit070@gmail.com>
2024-08-13 10:10:21 +0530

    update: crontab setup

--- setup/crontab.sh
+++ setup/crontab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "0 * * * * $HOME/dotfiles/scripts/update_brewfile.sh" >>mycron
+echo "0 * * * * $HOME/dotfiles/scripts/update_brewfile.sh >/dev/null 2>&1" >>mycron
 
 crontab -l >mycron
 
